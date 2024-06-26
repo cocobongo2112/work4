5cfc78d6f626634f76d6ab8d8949dc49dd287f8a
cocobongo2112 <felipegl.ti23@utsjr.edu.mx>
2024-06-25 18:52:02 -0600

    tercer commit

---
+++ bd/ConectarBD.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+class ConectarBD{
+    constructor(){
+        this.conexion=null
+        this.mysql=require("mysql2/promise");
+    }
+    async conectarMySql(){
+    try {
+        this.conexion=await this.mysql.createConnection({
+            host:process.env.HOSTMYSQL,
+            user:process.env.USERMYSQL,
+            password:process.env.PASSWORDMYSQL,
+            database:process.env.DATABASEMYSQL,
+            port:process.env.PORTMYSQL
+        });
+        console.log("Conexión creada a MySql");
+    } catch (error) {
+        console.error("Error al conectar con MySql"+error);
+    }
+    }
+    async cerrarConexion(){
+        try {
+            await this.conexion.end();
+            console.log("Desconexión de Mysql");
+        } catch (error) {
+            console.log("Error al desconectar de Mysql"+error);
+        }
+    }
+}
+module.exports=ConectarBD;
